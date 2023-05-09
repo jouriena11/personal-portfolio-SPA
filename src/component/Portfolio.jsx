05ce0b76f732fad52a08202c9afe2db981c07d76
jouriena11 <117335220+jouriena11@users.noreply.github.com>
2023-05-10 00:08:00 +1000

    revised porfolio props value

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -22,43 +22,43 @@ export default function Portfolio() {
           title="Chat Support"
           content="Node.js | Express.js | MySql | Socket.io | JavaScript"
           bgImgClass="flip-img-chat"
-          GitHubLink="https://chat-support-999.herokuapp.com/"
-          deployApp="https://github.com/jouriena11/chat-support"
+          GitHubLink="https://github.com/jouriena11/chat-support"
+          deployApp="https://chat-support-999.herokuapp.com/"
         />
         <ProjectCard
           title="Movie Search Engine"
           content="HTML | CSS | Bootstrap | JavaScript | jQuery | APIs"
           bgImgClass="flip-img-movie-search"
-          GitHubLink="https://e73707.github.io/Movie-search-engine/"
-          deployApp="https://github.com/E73707/Movie-search-engine"
+          GitHubLink="https://github.com/E73707/Movie-search-engine"
+          deployApp="https://e73707.github.io/Movie-search-engine/"
         />
         <ProjectCard
           title="Tech Blog"
           content="HTML | CSS | Bootstrap | JavaScript | Express.js | MySQL"
           bgImgClass="flip-img-tech-blog"
-          GitHubLink="https://tech-blog-999.herokuapp.com/"
-          deployApp="https://github.com/jouriena11/tech-blog-mvc"
+          GitHubLink="https://github.com/jouriena11/tech-blog-mvc"
+          deployApp="https://tech-blog-999.herokuapp.com/"
         />
         <ProjectCard
           title="PWA Text Editor"
           content="JavaScript | Express.js | Webpack | CodeMirror | IndexedDB"
           bgImgClass="flip-img-pwa-text-editor"
-          GitHubLink="https://jt-pwa-text-editor.herokuapp.com/"
-          deployApp="https://github.com/jouriena11/text-editor-PWA"
+          GitHubLink="https://github.com/jouriena11/text-editor-PWA"
+          deployApp="https://jt-pwa-text-editor.herokuapp.com/"
         />
         <ProjectCard
           title="Scheduler Calendar App"
           content="JavaScript | HTML | CSS | localStorage"
           bgImgClass="flip-img-pwa-scheduler-calendar-app"
-          GitHubLink="https://jouriena11.github.io/W5-challenge-scheduler-calendar-app/"
-          deployApp="https://github.com/jouriena11/W5-challenge-scheduler-calendar-app"
+          GitHubLink="https://github.com/jouriena11/W5-challenge-scheduler-calendar-app"
+          deployApp="https://jouriena11.github.io/W5-challenge-scheduler-calendar-app/"
         />
         <ProjectCard
           title="Weather Forecast App"
           content="JavaScript | HTML | CSS"
           bgImgClass="flip-img-pwa-weather-forecast-app"
-          GitHubLink="https://jouriena11.github.io/weather-forecast-by-city-dashboard/"
-          deployApp="https://github.com/jouriena11/weather-forecast-by-city-dashboard"
+          GitHubLink="https://github.com/jouriena11/weather-forecast-by-city-dashboard"
+          deployApp="https://jouriena11.github.io/weather-forecast-by-city-dashboard/"
         />
       </div>
     </div>
